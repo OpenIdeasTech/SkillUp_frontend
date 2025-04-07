cd0a8f898797dea5fe17c4962b7854adf160c7e8
Gabriela Jussana <gabrielajussana@gmail.com>
2025-04-07 13:59:49 -0300

    feat: main home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import MainHome from "@/components/MainHome";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex w-full h-screen bg-gradient-to-r from-[#9233E9] to-[#5045E5]">
+      <div className="flex flex-col w-full h-screen bg-gradient-to-r from-[#9233E9] to-[#5045E5] py-6 px-12 lg:px-24">
         <NavBar />
+        <MainHome />
       </div>
       
     </div>
