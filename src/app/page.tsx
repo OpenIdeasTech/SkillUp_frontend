27ce54058fb87a4b4242cb61d67c4f8435945b48
Gabriela Jussana <gabrielajussana@gmail.com>
2025-04-07 22:07:02 -0300

    feat: resources and projects list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+import Community from "@/components/Community";
 import MainHome from "@/components/MainHome";
 import NavBar from "@/components/NavBar";
+import Resources from "@/components/Resources";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex flex-col w-full h-screen bg-gradient-to-r from-[#9233E9] to-[#5045E5] py-6 px-12 lg:px-24">
+      <div className="flex flex-col w-full h-screen bg-gradient-to-r from-[#9233E9] to-[#5045E5] py-12 md:px-6  lg:px-[100px]">
         <NavBar />
         <MainHome />
       </div>
-      
+      <div className="bg-[#2D1C7F] w-full flex pt-[60px] md:px-6 lg:px-[100px]">
+        <Resources />
+      </div>
+      <div className="bg-white w-full flex pt-[60px] md:px-6 lg:px-[100px]">
+        <Community />
+      </div>
     </div>
   );
 }
--- src/components/MainHome.tsx
+++ src/components/MainHome.tsx
@@ -6,12 +6,12 @@ const MainHome = () => {
   return (
     <div className="flex flex-col justify-center w-full h-full text-white gap-6 md:text-left  py-16">
       
-      <h1 className="text-8xl font-bold font-instrumental-sans leading-tight">
+      <h1 className="text-8xl  font-bold font-instrumental-sans leading-none" >
         Destaque seu trabalho com <br className="hidden md:block" />
         um portfólio profissional
       </h1>
 
-      <h2 className="text-3xl font-inter">
+      <h2 className="text-3xl  font-inter">
         Crie um portfólio colaborativo, conecte-se com outras pessoas.
       </h2>
 
