1e36721f720d031cc9ff27775ddf4e039458ec07
Gabriela Jussana <gabrielajussana@gmail.com>
2025-04-16 12:45:57 -0300

    feat: config register page

---
+++ src/app/(register)/layout.tsx
@@ -0,0 +1,15 @@
+
+const RegisterLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="h-screen px=[100px]">
+          {children}
+        </div>
+    )
+}
+
+export default RegisterLayout;
+
